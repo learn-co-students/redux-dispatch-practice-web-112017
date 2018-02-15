8b1434eba8066925d4087be04bde4429dc8144fc
ddbrennan <ddbren@gmail.com>
2018-02-14 22:28:57 -0500

    dispatch render working

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={ pets:[] }, action){
+  switch(action.type) {
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      let newPets = state.pets.filter(p => p.id !== action.id)
+      return { pets: newPets }
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let main = document.getElementById('container')
+  let lis = state.pets.map(p => `<li>${p.name}</li>`)
+  main.innerHTML = "<ul>" + lis + "</ul>"
 }
