da9e2e9e9c13ccd6a0201faa84e09aa0a00caf4a
L-A-Davis <lindsay.amanda.davis@gmail.com>
2018-02-20 13:41:17 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,42 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return {
+        ...state,
+        pets: [
+          ...state.pets,
+          action.pet
+          ]
+         }
+    case 'REMOVE_PET':
+     let petIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return {
+        ...state,
+        pets: [
+          ...state.pets.slice(0, petIndex),
+          ...state.pets.slice(petIndex + 1)
+        ]
+      }
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render ()
+
 }
 
 export function render(){
-
+   let body = document.getElementById("main")
+   let list = state.pets.map((pet) => {
+     return `<li>${pet.name} </li>`
+   }).join(' ');
+   body.innerHTML = `<ul>${list}</ul>`
 }
+
+dispatch({type: '@@INIT'})
